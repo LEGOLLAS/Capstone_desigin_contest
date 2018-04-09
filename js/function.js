63cf772478b7f9514dbe36a94bc5ca765d7fa092
LEGOLLAS <gurwl1885@gmail.com>
2018-04-09 11:59:07 +0900

    add js/jquery

--- js/function.js
+++ js/function.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-			$('#Menu_bt, #submenu').hover(function() {
-			$('#submenu').show();
+			$('#Menu_bt, #Submenu').hover(function() {
+			$('#Submenu').show();
 		}, function () {
-			$('#submenu').hide();
+			$('#Submenu').hide();
 		});
 });
